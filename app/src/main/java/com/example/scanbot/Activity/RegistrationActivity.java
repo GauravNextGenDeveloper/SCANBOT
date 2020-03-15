21e1b867bbed73ac106ab725d3680640f4e4ab1a
gaurav <gauravnaik309@gmail.com>
2020-03-15 13:19:43 +0530

    //Permission Code

--- app/src/main/java/com/example/scanbot/Activity/RegistrationActivity.java
+++ app/src/main/java/com/example/scanbot/Activity/RegistrationActivity.java
@@ -2,7 +2,9 @@ package com.example.scanbot.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -16,8 +18,11 @@ import com.example.scanbot.Database.ScanBotDatabase;
 import com.example.scanbot.Database.model.Register;
 import com.example.scanbot.R;
 import com.google.android.material.textfield.TextInputEditText;
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -105,6 +110,26 @@ public class RegistrationActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+        String[] permissions = {Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE};
+        String rationale = "Please provide location permission so that you can ...";
+        Permissions.Options options = new Permissions.Options()
+                .setRationaleDialogTitle("Info")
+                .setSettingsDialogTitle("Warning");
+
+        Permissions.check(this/*context*/, permissions, rationale, options, new PermissionHandler() {
+            @Override
+            public void onGranted() {
+                // do your task.
+            }
+
+            @Override
+            public void onDenied(Context context, ArrayList<String> deniedPermissions) {
+                // permission denied, block the feature.
+            }
+        });
+
     }
 
 
